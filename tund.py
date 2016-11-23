2850b386581ab669ae0777492cc371ebbbee7561
tambettelvis <tambetttanilsoo@gmail.com>
2016-11-23 16:38:04 +0200

    Update tund.py

--- tund.py
+++ tund.py
@@ -4,25 +4,20 @@ class Tund:
 
     tunnid = []#Koosneb kõigist tundidest.
     
-    def __init__(self, lessonName, location, beginDateTime, endDateTime, color):
+    def __init__(self, lessonName, location, weekday, time, color):
         self.lessonName = lessonName
         self.location = location
-        self.beginDateTime = beginDateTime #Formaat YYYY-MM-dd HH:mm:ss
-        self.endDateTime = endDateTime
+        self.weekday = weekday
+        self.time = time
         self.color = color
-
         Tund.tunnid.append(self)
 
 
-    def getBeginDate(self):
-        return self.beginDateTime.split()[0] #Formaat YYYY-MM-dd
+    def getWeekday(self):
+        return self.weekday
 
-    def getBeginTime(self):
-        return self.beginDateTime.split()[1] #Formaat HH:mm:ss
-
-    def getEndDate(self):
-        return self.endDateTime.split()[0] #Formaat YYYY-MM-dd
-
-    def getEndTime(self):
-        return self.endDateTime.split()[1] #Formaat HH:mm:ss
+    def getTime(self):
+        return self.time
     
+    def getColor(self):
+        return self.color
