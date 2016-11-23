633c9de0126e83ad1a416a0add237c475fd27324
tambettelvis <tambetttanilsoo@gmail.com>
2016-11-23 17:16:37 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,17 +1,29 @@
-
+import tund
 
 class Task:
 
-    staticVar = 5
-
-    def __init__(self, ajakulu, finishTime, priority, color):
+    tasks = []
+    
+    def __init__(self, name, ajakulu, finishTime, timeSteps, priority, color):
         self.ajakulu = ajakulu
         self.finishTime = finishTime
+        self.timeSteps = timeSteps
         self.priority = priority
         self.color = color
-
+        self.timesList = otsiVabadAjad()#Formaat list[] HH:mm
+        Task.tasks(self)
+    
+    def getAjakulu(self):
+        return self.ajakulu
+    
+    def getName(self):
+        return self.name
+    
+    def getFinishTime(Self):
+        return self.finishTime
+    
     def otsiVabadAjad(self):
-        listOfFreeTimes = []
+        for Tund...mitte puutuda!!!
 
         
         
