66a9b9c73a593c329f26e64ed45bef0b9035f340
tambettelvis <tambetttanilsoo@gmail.com>
2016-12-14 20:19:10 +0200

    Add files via upload

--- calander.py
+++ calander.py
@@ -12,6 +12,7 @@ def createEventList(url):
         ...'''
     for event in c.events:
         #days = ["Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"]
+        print(event.name + " " + event.begin.format(timeFormat) + " " +event.begin.format(timeFormat2) + " WEEKDAY:" + str(event.begin.weekday()))
         tund.Tund(event.name, event.location, event.begin.weekday(), event.begin.format(timeFormat), event.description, event.begin.format(timeFormat2), 'red')
     
 #events = getEventsList("http://www.is.ut.ee/pls/ois/ois.kalender?id_kalender=1228100092")
